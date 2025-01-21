980c7001cb67a5536a7056c89cf28bef89ac487b
Wasim Zaman <wasimxaman13@gmail.com>
2025-01-21 10:02:49 +0500

    Refactor routes.js to remove unused route imports

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import express from "express";
 
-import departmentRoutes from "./routes/department.js";
-import kpiRoutes from "./routes/kpi.js";
-import patientRoutes from "./routes/patient.js";
 import userRoutes from "./routes/user.js";
 
 const router = express.Router();
 
 router.use("/user", userRoutes);
-router.use("/patients", patientRoutes);
-router.use("/departments", departmentRoutes);
-router.use("/kpi", kpiRoutes);
 
 export default router;
