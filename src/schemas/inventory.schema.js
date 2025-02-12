b5feb190d60efb66f393a70aa36d31c7cc56f66f
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2025-02-12 14:11:46 +0500

    refactor: Make inventory schema fields optional

--- src/schemas/inventory.schema.js
+++ src/schemas/inventory.schema.js
@@ -3,13 +3,13 @@ import Joi from "joi";
 const inventoryTypes = ["Assset", "Consumable"];
 
 export const inventorySchema = Joi.object({
-  assetLocation: Joi.string().required(),
-  mainCatCode: Joi.string().required(),
-  mainCatDesc: Joi.string().required(),
-  mainDesc: Joi.string().required(),
-  subCatCode: Joi.string().required(),
-  subCatDesc: Joi.string().required(),
-  assetCategory: Joi.string().required(),
+  assetLocation: Joi.string(),
+  mainCatCode: Joi.string(),
+  mainCatDesc: Joi.string(),
+  mainDesc: Joi.string(),
+  subCatCode: Joi.string(),
+  subCatDesc: Joi.string(),
+  assetCategory: Joi.string(),
   image: Joi.string().optional(),
   quantity: Joi.number().integer().min(1).default(1),
   serial: Joi.string().optional(),
