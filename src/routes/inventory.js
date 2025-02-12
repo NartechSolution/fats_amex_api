a06369b064a309ea2a82d27764965bbdbebb02dd
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2025-02-12 14:08:19 +0500

    fixed the directory

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -1,18 +1,24 @@
 import express from "express";
 import { uploadSingle } from "multermate-es";
 import InventoryController from "../controllers/inventory.js";
+import { ensureDir } from "../utils/file.js";
+
 const router = express.Router();
 const upload = uploadSingle({
   destination: "uploads/inventory",
   filename: "image",
 });
 
+const ensureUpload = (req, res, next) => {
+  ensureDir("uploads/inventory");
+  next();
+};
 // router.use(verifyAccessToken);
 
-router.post("/", upload, InventoryController.create);
+router.post("/", ensureUpload, InventoryController.create);
 router.get("/", InventoryController.getAll);
 router.get("/:id", InventoryController.getById);
-router.put("/:id", upload, InventoryController.update);
+router.put("/:id", ensureUpload, upload, InventoryController.update);
 router.delete("/:id", InventoryController.delete);
 
 export default router;
